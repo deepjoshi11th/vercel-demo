6d2baf6d2fab2c3e6468db2751059741db5e086e
Deep Joshi <deepjoshi11th@gmail.com>
2026-04-12 23:54:27 +0530

    fix: update environment variable for Supabase key to SUPABASE_SECRET_KEY

--- poc/supabase_connection.py
+++ poc/supabase_connection.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client, Client
 
 url:str = str(os.environ.get("SUPABASE_URL"))
-key:str = str(os.environ.get("SUPABASE_KEY"))
+key:str = str(os.environ.get("SUPABASE_SECRET_KEY"))
 supabase: Client = create_client(url, key)
 response = supabase.table("sample_table").select("*").execute()
 print(response)
