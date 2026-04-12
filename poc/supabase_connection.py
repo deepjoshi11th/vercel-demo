278f7ed9193e2a4cf90b096c42f60a82a2dc5b61
Deep Joshi <deepjoshi11th@gmail.com>
2026-04-12 16:24:08 +0530

    feat: add supabase_connection.py to connect and fetch data from Supabase

---
+++ poc/supabase_connection.py
@@ -0,0 +1,8 @@
+import os
+from supabase import create_client, Client
+
+url:str = str(os.environ.get("SUPABASE_URL"))
+key:str = str(os.environ.get("SUPABASE_KEY"))
+supabase: Client = create_client(url, key)
+response = supabase.table("sample_table").select("*").execute()
+print(response)
