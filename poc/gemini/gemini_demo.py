6e422b7cd55bb660c0f2cd89a09c079657748916
Deep Joshi <deepjoshi11th@gmail.com>
2026-04-22 10:25:09 +0530

    fix: path change in gemini demo

--- poc/gemini/gemini_demo.py
+++ poc/gemini/gemini_demo.py
@@ -13,11 +13,11 @@ class Question(BaseModel):
 if __name__ == "__main__":
     client = genai.Client(api_key=GEMINI_API_KEY)
     system_instruction = ""
-    with open("poc/system-instruction.md", "r") as f:
+    with open("poc/gemini/system-instruction.md", "r") as f:
         lines = f.readlines()
         for line in lines:
             system_instruction += line
-    print(system_instruction)
+    # print(system_instruction)
     response = client.models.generate_content(
         model="gemini-3-flash-preview",
         contents="I am working on a Java server which have the remove background functionality which is executed on backend, hosted on serverless platform like vercel. The image output is png files without watermark but with 512KB of input size limit. VCS is github.",
